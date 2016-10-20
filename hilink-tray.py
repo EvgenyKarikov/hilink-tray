3b6f0eb892c53508bb887f81cfbbb479338cc961
Wasylews <ua.wasylews@gmail.com>
2016-10-20 19:36:38 +0300

    Test version 4.0

--- hilink-tray.py
+++ hilink-tray.py
@@ -7,137 +7,10 @@
 from __future__ import print_function
 import sys
 import signal
-import socket
 import argparse
 from PySide import QtCore, QtGui
 from PySide.phonon import Phonon
-from xml.etree import ElementTree
-from collections import OrderedDict
-
-try:
-    import res_rc
-    from urllib2 import build_opener, URLError
-    from urlparse import urljoin
-except ImportError:  # >= 3.x
-    import res3_rc
-    from urllib.request import build_opener
-    from urllib.error import URLError
-    from urllib.parse import urljoin
-
-
-class ModemMonitor(QtCore.QThread):
-    """ModemMonitor - class for monitoring modem parameters"""
-    levelChanged = QtCore.Signal(int)
-    statusChanged = QtCore.Signal(str, str)
-    signalParamsChanged = QtCore.Signal(OrderedDict)
-    unreadMessagesCountChanged = QtCore.Signal(int)
-
-    def __init__(self, ip, timeout):
-        super(ModemMonitor, self).__init__()
-
-        self._url = "http://{}".format(ip)
-        self._timeout = timeout
-        self._running = True
-
-    def stop(self):
-        self._running = False
-
-    def _getXml(self, opener, section):
-        response = opener.open(urljoin(self._url, section), timeout=1)
-        return ElementTree.fromstring(response.read())
-
-    def getCookie(self, opener):
-        """Get access token"""
-        try:
-            xml = self._getXml(opener, "/api/webserver/SesTokInfo")
-        except URLError:
-            return ""
-        else:
-            return xml.find("SesInfo").text
-
-    def getSignalLevel(self, xml):
-        return int(xml.find("SignalIcon").text)
-
-    def getNetworkType(self, xml):
-        types = {"0": "No Service", "1": "GSM", "2": "GPRS", "3": "EDGE",
-                 "21": "IS-95A", "22": "IS-95B", "23": "CDMA 1X", "24": "EV-DO Rev. 0", "25": "EV-DO Rev. A", "26": "EV-DO Rev. A", "27": "Hybrid CDMA 1X", "28": "Hybrid EV-DO Rev. 0", "29": "Hybrid EV-DO Rev. A", "30": "Hybrid EV-DO Rev. A", "31": "eHPRD Rel. 0", "32": "eHPRD Rel. A", "33": "eHPRD Rel. B", "34": "Hybrid eHPRD Rel. 0", "35": "Hybrid eHPRD Rel. A", "36": "Hybrid eHPRD Rel. B",
-                 "41": "WCDMA", "42": "HSDPA", "43": "HSUPA", "44": "HSPA", "45": "HSPA+", "46": "DC-HSPA+",
-                 "61": "TD-SCDMA", "62": "TD-HSDPA", "63": "TD-HSUPA", "64": "TD-HSPA", "65": "TD-HSPA+",
-                 "81": "802.16e",
-                 "101": "LTE"}
-        return types[xml.find("CurrentNetworkTypeEx").text]
-
-    def getStatus(self, xml):
-        states = {"900": "Connecting", "901": "Connected",
-                  "902": "Disconnected", "903": "Disconnecting"}
-        return states[xml.find("ConnectionStatus").text]
-
-    def getOperator(self, xml):
-        return xml.find("FullName").text
-
-    def getSignalParams(self, xml):
-        params = ["rssi", "rsrp", "rsrq", "rscp", "ecio", "sinr",
-                  "cell_id", "pci"]
-        values = OrderedDict()
-
-        for key in params:
-            values[key] = "{key}: {val}".format(key=key.upper(),
-                                                val=xml.find(key).text)
-        return values
-
-    def getUnreadMessageCount(self, xml):
-        """Get number of unreaded messages"""
-        return int(xml.find("UnreadMessage").text)
-
-    def monitorMessages(self, opener):
-        """Monitor count of unreaded messages"""
-        try:
-            notifyXml = self._getXml(opener,
-                                     "/api/monitoring/check-notifications")
-            messageCount = self.getUnreadMessageCount(notifyXml)
-        except:
-            self.unreadMessagesCountChanged.emit(0)
-        else:
-            self.unreadMessagesCountChanged.emit(messageCount)
-
-    def monitorStatus(self, opener):
-        """Monitor connection status"""
-        try:
-            statusXml = self._getXml(opener, "/api/monitoring/status")
-            plmnXml = self._getXml(opener, "/api/net/current-plmn")
-        except:
-            self.levelChanged.emit(0)
-            self.statusChanged.emit("Disconnected", "")
-        else:
-            signalLevel = self.getSignalLevel(statusXml)
-            self.levelChanged.emit(signalLevel)
-
-            status = self.getStatus(statusXml)
-            networkType = self.getNetworkType(statusXml)
-            operator = self.getOperator(plmnXml)
-            self.statusChanged.emit(status, "%s %s" % (operator, networkType))
-
-    def monitorSignalParams(self, opener):
-        """Monitor signal parameters"""
-        try:
-            signalXml = self._getXml(opener, "/api/device/signal")
-        except:
-            self.signalParamsChanged.emit({})
-        else:
-            params = self.getSignalParams(signalXml)
-            self.signalParamsChanged.emit(params)
-
-    def run(self):
-        opener = build_opener()
-        cookie = self.getCookie(opener)
-        opener.addheaders.append(("Cookie", cookie))
-
-        while self._running:
-            self.monitorMessages(opener)
-            self.monitorStatus(opener)
-            self.monitorSignalParams(opener)
-
-            self.sleep(self._timeout)
+from hilink.modem import Modem
 
 
 class ModemIndicator(QtGui.QSystemTrayIcon):
@@ -149,35 +22,47 @@ class ModemIndicator(QtGui.QSystemTrayIcon):
     # is user notified or not
     _notified = False
 
-    def __init__(self, monitor):
+    def __init__(self, modem):
         super(ModemIndicator, self).__init__()
+        self._modem = modem
         self.signalLevelChanged(0)
         menu = self.createMenu()
         self.setContextMenu(menu)
-        self._monitor = monitor
 
         self.player = Phonon.createPlayer(Phonon.MusicCategory)
         self.player.stateChanged.connect(self._playerLog)
 
     def createMenu(self):
         menu = QtGui.QMenu()
+
+        self.connectAction = QtGui.QAction("Connect", menu)
+        self.connectAction.triggered.connect(self._modem.connect_)
+        menu.addAction(self.connectAction)
+
+        self.disconnectAction = QtGui.QAction("Disconnect", menu)
+        self.disconnectAction.triggered.connect(self._modem.disconnect)
+        self.disconnectAction.setDisabled(True)
+        menu.addAction(self.disconnectAction)
+
+        self.rebootAction = QtGui.QAction("Reboot", menu)
+        self.rebootAction.triggered.connect(self._modem.reboot)
+        self.rebootAction.setDisabled(True)
+        menu.addAction(self.rebootAction)
+
+        menu.addSeparator()
+
         quitAction = QtGui.QAction("Quit", menu)
-        quitAction.triggered.connect(self.close)
+        quitAction.triggered.connect(self.quit)
         menu.addAction(quitAction)
+
         return menu
 
-    def close(self):
+    def quit(self):
         self.hide()
-        self._monitor.stop()
-        if not self._monitor.wait(5000):
-            self._monitor.terminate()
-            self._monitor.wait()
-        QtGui.qApp.quit()
+        self._modem.finish()
 
     def signalLevelChanged(self, level):
-        if self._notified:
-            return
-
+        print(level)
         iconName = "://images/icons/icon_signal_00.png"
         if level in range(1, 6):
             iconName = "://images/icons/icon_signal_0{}.png".format(level)
@@ -187,19 +72,24 @@ class ModemIndicator(QtGui.QSystemTrayIcon):
     def needNotify(self, messageCount):
         if messageCount > 0:
             if messageCount > self._lastMessageCount:
-                print("notify")
                 self._playSound()
-
             iconName = "://images/icons/unread_message.gif"
             self.setIcon(QtGui.QIcon(iconName))
-
-            self._notified = True
-        else:
-            self._notified = False
 
         self._lastMessageCount = messageCount
 
     def statusChanged(self, status, operator):
+        if status == "Connected":
+            self.connectAction.setDisabled(True)
+            self.disconnectAction.setEnabled(True)
+            self.rebootAction.setEnabled(True)
+            self._modem.start()
+        elif status == "Disconnected":
+            self.connectAction.setEnabled(True)
+            self.disconnectAction.setDisabled(True)
+            self.rebootAction.setDisabled(True)
+            self._modem.stop()
+
         if operator != "":
             self.setToolTip("%s\n%s" % (status, operator))
         else:
@@ -207,9 +97,8 @@ class ModemIndicator(QtGui.QSystemTrayIcon):
 
     def signalParamsChanged(self, params):
         tip = [self.toolTip()]
-        for (key, value) in params.items():
-            if value:
-                tip.append(value)
+        for value in params.values():
+            tip.append(value)
 
         self.setToolTip("\n".join(tip))
 
@@ -227,16 +116,35 @@ def main(ip, timeout):
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QtGui.QApplication(sys.argv)
 
-    monitor = ModemMonitor(ip, timeout)
+    monitorThread = QtCore.QThread()
+    modem = Modem(ip)
+    modem.moveToThread(monitorThread)
 
-    trayIndicator = ModemIndicator(monitor)
-    monitor.levelChanged.connect(trayIndicator.signalLevelChanged)
-    monitor.statusChanged.connect(trayIndicator.statusChanged)
-    monitor.signalParamsChanged.connect(trayIndicator.signalParamsChanged)
-    monitor.unreadMessagesCountChanged.connect(trayIndicator.needNotify)
+    modem.finished.connect(monitorThread.quit)
+    monitorThread.finished.connect(app.quit)
 
-    monitor.start()
+    timer = QtCore.QTimer()
+    timer.setInterval(timeout)
+    timer.moveToThread(monitorThread)
+
+    monitorThread.started.connect(modem.start)
+    timer.timeout.connect(modem.monitor)
+
+    modem.started.connect(timer.start)
+    modem.stopped.connect(timer.stop)
+
+    trayIndicator = ModemIndicator(modem)
+    modem.levelChanged.connect(
+        trayIndicator.signalLevelChanged, QtCore.Qt.QueuedConnection)
+    modem.statusChanged.connect(
+        trayIndicator.statusChanged, QtCore.Qt.QueuedConnection)
+    modem.signalParamsChanged.connect(
+        trayIndicator.signalParamsChanged, QtCore.Qt.QueuedConnection)
+    modem.unreadMessagesCountChanged.connect(
+        trayIndicator.needNotify, QtCore.Qt.QueuedConnection)
+
     trayIndicator.show()
+    monitorThread.start()
 
     return app.exec_()
 
@@ -259,11 +167,11 @@ def parseArgs():
 
     parser.add_argument(
         "-v", "--version",
-        action="version", version="%(prog)s 3.1")
+        action="version", version="%(prog)s 4.0")
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parseArgs()
-    sys.exit(main(args.ip, args.timeout))
+    sys.exit(main(args.ip, args.timeout * 1000))
---
+++ hilink/modem.py
@@ -0,0 +1,172 @@
+from PySide import QtCore
+from xml.etree import ElementTree
+from collections import OrderedDict
+
+try:
+    import hilink.res_rc
+    from cookielib import CookieJar
+    from urllib2 import build_opener, URLError, HTTPCookieProcessor
+    from urlparse import urljoin
+except ImportError:  # >= 3.x
+    import hilink.res3_rc
+    from http.cookie import CookieJar
+    from urllib.request import build_opener, HTTPCookieProcessor
+    from urllib.error import URLError
+    from urllib.parse import urljoin
+
+
+class Modem(QtCore.QObject):
+    """Modem - class that represents HiLink modem"""
+    levelChanged = QtCore.Signal(int)
+    statusChanged = QtCore.Signal(str, str)
+    signalParamsChanged = QtCore.Signal(OrderedDict)
+    unreadMessagesCountChanged = QtCore.Signal(int)
+    finished = QtCore.Signal()
+    started = QtCore.Signal()
+    stopped = QtCore.Signal()
+
+    def __init__(self, ip):
+        super(Modem, self).__init__()
+        self._session = CookieJar()
+        self._opener = build_opener(HTTPCookieProcessor(self._session))
+        self._baseUrl = "http://{}".format(ip)
+
+    def finish(self):
+        self.finished.emit()
+
+    def start(self):
+        self.started.emit()
+
+    def stop(self):
+        self.stopped.emit()
+
+    def connect_(self):
+        msg = """<?xml version="1.0" encoding="UTF-8"?><request><dataswitch>1</dataswitch></request>"""
+
+        self._post("/api/dialup/mobile-dataswitch", msg)
+
+    def disconnect(self):
+        msg = """<?xml version="1.0" encoding="UTF-8"?><request><dataswitch>0</dataswitch></request>"""
+
+        self._post("/api/dialup/mobile-dataswitch", msg)
+
+    def reboot(self):
+        msg = """<?xml version="1.0" encoding="UTF-8"?><request><Control>1</Control></request>"""
+        self._post("/api/device/control", msg)
+
+    def _post(self, section, msg):
+        token = self._getPostToken()
+        self._opener.addheaders.append(("__RequestVerificationToken", token))
+        self._opener.open(urljoin(self._baseUrl, section),
+                          data=msg)
+
+    def _getXml(self, section):
+        response = self._opener.open(urljoin(self._baseUrl, section),
+                                     timeout=1)
+        return ElementTree.fromstring(response.read())
+
+    def _getPostToken(self):
+        """Get access token"""
+        try:
+            xml = self._getXml("/api/webserver/SesTokInfo")
+        except URLError:
+            return ""
+        else:
+            return xml.find("TokInfo").text
+
+    def getSignalLevel(self, xml):
+        return int(xml.find("SignalIcon").text)
+
+    def getNetworkType(self, xml):
+        types = {"0": "No Service", "1": "GSM", "2": "GPRS", "3": "EDGE",
+                 "21": "IS-95A", "22": "IS-95B", "23": "CDMA 1X",
+                 "24": "EV-DO Rev. 0", "25": "EV-DO Rev. A",
+                 "26": "EV-DO Rev. A", "27": "Hybrid CDMA 1X",
+                 "28": "Hybrid EV-DO Rev. 0", "29": "Hybrid EV-DO Rev. A",
+                 "30": "Hybrid EV-DO Rev. A", "31": "eHPRD Rel. 0",
+                 "32": "eHPRD Rel. A", "33": "eHPRD Rel. B",
+                 "34": "Hybrid eHPRD Rel. 0", "35": "Hybrid eHPRD Rel. A",
+                 "36": "Hybrid eHPRD Rel. B", "41": "WCDMA", "42": "HSDPA",
+                 "43": "HSUPA", "44": "HSPA", "45": "HSPA+", "46": "DC-HSPA+",
+                 "61": "TD-SCDMA", "62": "TD-HSDPA", "63": "TD-HSUPA",
+                 "64": "TD-HSPA", "65": "TD-HSPA+", "81": "802.16e",
+                 "101": "LTE"}
+        return types[xml.find("CurrentNetworkTypeEx").text]
+
+    def getStatus(self, xml):
+        states = {"900": "Connecting", "901": "Connected",
+                  "902": "Disconnected", "903": "Disconnecting"}
+        return states[xml.find("ConnectionStatus").text]
+
+    def getOperator(self, xml):
+        shortName = xml.find("ShortName")
+        if shortName is not None:
+            return shortName.text
+        else:
+            fullName = xml.find("FullName")
+            if fullName is not None:
+                return fullName.text
+        return ""
+
+    def getSignalParams(self, xml):
+        params = ["rssi", "rsrp", "rsrq", "rscp", "ecio", "sinr",
+                  "cell_id", "pci"]
+        values = OrderedDict()
+
+        for key in params:
+            value = xml.find(key).text
+            if value:
+                values[key] = "{key}: {val}".format(key=key.upper(),
+                                                    val=value)
+        return values
+
+    def getUnreadMessageCount(self, xml):
+        """Get number of unreaded messages"""
+        return int(xml.find("UnreadMessage").text)
+
+    def monitorMessages(self):
+        """Monitor count of unreaded messages"""
+        try:
+            notifyXml = self._getXml("/api/monitoring/check-notifications")
+            messageCount = self.getUnreadMessageCount(notifyXml)
+        except:
+            self.unreadMessagesCountChanged.emit(0)
+        else:
+            self.unreadMessagesCountChanged.emit(messageCount)
+
+    def monitorStatus(self):
+        """Monitor connection status"""
+        try:
+            statusXml = self._getXml("/api/monitoring/status")
+            plmnXml = self._getXml("/api/net/current-plmn")
+        except:
+            self.levelChanged.emit(0)
+            self.statusChanged.emit("Disconnected", "")
+        else:
+            signalLevel = self.getSignalLevel(statusXml)
+            self.levelChanged.emit(signalLevel)
+
+            status = self.getStatus(statusXml)
+            networkType = self.getNetworkType(statusXml)
+            operator = self.getOperator(plmnXml)
+
+            # fix space if we don't have op name
+            op = networkType
+            if operator:
+                op = "%s %s" % (operator, networkType)
+            self.statusChanged.emit(status, op)
+
+    def monitorSignalParams(self):
+        """Monitor signal parameters"""
+        try:
+            signalXml = self._getXml("/api/device/signal")
+        except:
+            self.signalParamsChanged.emit({})
+        else:
+            params = self.getSignalParams(signalXml)
+            self.signalParamsChanged.emit(params)
+
+    def monitor(self):
+        self.monitorMessages()
+        self.monitorStatus()
+        self.monitorSignalParams()
